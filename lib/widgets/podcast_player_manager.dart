e3fb63cec627577a0357e15b8aaa9cf41d0122de
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-05-09 10:16:15 +0000

    minor fixes

---
+++ lib/widgets/podcast_player_manager.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class PageManager {
+  final progressNotifier = ValueNotifier<ProgressBarState>(
+    ProgressBarState(
+      current: Duration.zero,
+      buffered: Duration.zero,
+      total: Duration(seconds: 10),
+    ),
+  );
+  final buttonNotifier = ValueNotifier<ButtonState>(ButtonState.paused);
+}
+
+class ProgressBarState {
+  ProgressBarState({
+    required this.current,
+    required this.buffered,
+    required this.total,
+  });
+  final Duration current;
+  final Duration buffered;
+  final Duration total;
+}
+
+enum ButtonState { paused, playing, loading }
--- lib/widgets/podcast_widget.dart
+++ lib/widgets/podcast_widget.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/blocs/podcast_bloc.dart';
 import 'package:news_app/models/podcast.dart';
+import 'package:provider/provider.dart';
 
 class PodcastWidget extends StatelessWidget {
   final PodcastModel podcastDetails;
-  const PodcastWidget({Key? key, required this.podcastDetails})
-      : super(key: key);
+  PodcastWidget({Key? key, required this.podcastDetails}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final cb = context.watch<PodcastBloc>();
+
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         CircleAvatar(
-          child: Icon(Icons.account_box),
+          backgroundImage: NetworkImage(podcastDetails.imageUrl),
         ),
         Expanded(
           child: Container(
@@ -23,7 +26,17 @@ class PodcastWidget extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(podcastDetails.title),
-                    Text(podcastDetails.duration.toString())
+                    cb.currentPodcast != null
+                        ? cb.currentPodcast!.title == podcastDetails.title
+                            // If the titles match, check if the podcast is playing
+                            ? cb.isPlaying
+                                // If the podcast is playing, show a pause icon
+                                ? Icon(Icons.pause)
+                                // If the podcast is not playing, show a play icon
+                                : Icon(Icons.play_arrow)
+                            // If the titles do not match, show a play icon
+                            : Icon(Icons.play_arrow)
+                        : Icon(Icons.play_arrow),
                   ],
                 ),
                 SizedBox(
